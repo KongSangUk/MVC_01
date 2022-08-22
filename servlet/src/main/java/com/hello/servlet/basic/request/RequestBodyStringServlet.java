9eb2b910da7eab81d4a75694aa5f7c1694e0e408
Kong Sang Uk <qkqhfp19@naver.com>
2022-08-23 01:08:21 +0900

    feat : RequestBodyStringServlet

--- servlet/src/main/java/com/hello/servlet/basic/request/RequestBodyStringServlet.java
+++ servlet/src/main/java/com/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -17,8 +17,7 @@ public class RequestBodyStringServlet extends HttpServlet {
 
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         ServletInputStream inputStream = request.getInputStream();
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
